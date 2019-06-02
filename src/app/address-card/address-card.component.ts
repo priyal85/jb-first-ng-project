6ce501e8eb7a8ce67a7e193be5d1924b5c0d26ad
Priyal <priyal85@gmail.com>
2019-06-02 12:25:40 +0530

    NgFor example added

---
+++ src/app/address-card/address-card.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-address-card',
+  templateUrl: './address-card.component.html',
+  styleUrls: ['./address-card.component.css']
+})
+export class AddressCardComponent implements OnInit {
+
+ user: any;
+
+  constructor() {
+    this.user = {
+      name: 'Bob Martin',
+      title: 'SE',
+      address: '520, Colombo 03',
+      phone: ['0112531014', '01137205289']
+  
+    };
+
+   }
+
+  ngOnInit() {
+  }
+
+}
